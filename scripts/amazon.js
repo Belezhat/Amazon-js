e49871c13aa38a9ca32b6db4cf7a61dfcd411eae
Bléza PLEGNON <bleza.plg@gmail.com>
2024-04-05 15:46:56 +0200

    Modification apportés au fichier amazon.html

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,3 @@
-const products = [{
-        image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name: 'Chaussettes athlétiques en coton noires et grises - 6 paires',
-        rating: {
-            stars: 4.5,
-            count: 87,
-        },
-        priceCents: 1090
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Ballon de basket de taille intermédiaire',
-    rating: {
-        stars: 4,
-        count: 127,
-    },
-    priceCents: 2095
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'T-shirt uni en coton pour adultes - Lot de 2',
-    rating: {
-        stars: 4.5,
-        count: 56,
-    },
-    priceCents: 799
-}];
-
 let productsHTML = '';
 
 products.forEach((product) => {
