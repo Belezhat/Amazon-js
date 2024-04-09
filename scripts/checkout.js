50708faccf8609f46e9719a339b9bc0dfd6eb2ce
Bléza PLEGNON <bleza.plg@gmail.com>
2024-04-09 15:05:20 +0200

    traitement du boutton supprimer

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
@@ -44,7 +44,8 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Modifier
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary 
+                  js-delete-link" data-product-id="${matchingProduct.id}">
                     Supprimer
                   </span>
                 </div>
@@ -102,4 +103,11 @@ cart.forEach((cartItem) => {
 document.querySelector('.js-order-summary')
 .innerHTML = cartSummaryHTML;
 
-console.log(cartSummaryHTML);
+document.querySelectorAll('.js-delete-link')
+.forEach((link) => {
+  link.addEventListener('click', () =>{
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+    
+  });
+});
