c18ef63e8ecff663017b7ec67300208e822e6798
Bléza PLEGNON <bleza.plg@gmail.com>
2024-04-09 17:59:25 +0200

    mise à jour fichier checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -21,7 +21,8 @@ cart.forEach((cartItem) => {
     cartSummaryHTML +=
 
     `
-    <div class="cart-item-container">
+    <div class="cart-item-container 
+    js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
                 Date de livraison : mardi 21 juin
             </div>
@@ -108,6 +109,11 @@ document.querySelectorAll('.js-delete-link')
   link.addEventListener('click', () =>{
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+      );
+      container.remove();
   });
+  
 });
